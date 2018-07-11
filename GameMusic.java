5c5d99c95a9ec21ca67efce3f296c26593712442
Ben <liftport@gmail.com>
2018-07-11 18:20:32 -0500

    Added button listeners.

--- GameMusic.java
+++ GameMusic.java
@@ -4,8 +4,6 @@ public class GameMusic {
     public static void main(String[] args) {
         UserInterface frame = new UserInterface();
 
-        MusicCollector collector = new MusicCollector();
 
-        collector.collectMusic();
     }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -3,17 +3,19 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class UserInterface extends JFrame {
-    JPanel listPanel = new JPanel();
-    JPanel controls = new JPanel();
+    private JPanel listPanel = new JPanel();
+    private JPanel controls = new JPanel();
 
-    JButton play = new JButton("Play");
-    JButton stop = new JButton("Stop");
+    private JButton play = new JButton("Play");
+    private JButton stop = new JButton("Stop");
 
-    JTable list;
+    private JTable list;
 
-    JScrollPane scrollPane = new JScrollPane();
+    private JScrollPane scroller = new JScrollPane();
 
     UserInterface() {
         setTitle("Music Player");
@@ -21,9 +23,7 @@ class UserInterface extends JFrame {
         setSize(new Dimension(800, 600));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
-
-        // Populate track table.
+        // Create table of songs.
         MusicCollector music = new MusicCollector();
         String[] columnNames = {"Song Name"};
         Object[][] tracks = music.collectMusic();
@@ -42,21 +42,21 @@ class UserInterface extends JFrame {
 
         listPanel.setBackground(Color.BLACK);
         listPanel.setLayout(new BorderLayout());
-
         add(listPanel, BorderLayout.CENTER);
 
         // Add scrollable music list to list panel.
-        JScrollPane scroller = new JScrollPane();
         scroller.setBackground(Color.BLACK);
         scroller.setViewportView(list);
         scroller.getViewport().setBackground(Color.BLACK);
         scroller.setBorder(BorderFactory.createRaisedBevelBorder());
         listPanel.add(scroller, BorderLayout.CENTER);
 
-        add(controls, BorderLayout.PAGE_END);
         controls.setBackground(Color.BLACK);
+        play.addActionListener(new ButtonListener());
         controls.add(play);
+        stop.addActionListener(new ButtonListener());
         controls.add(stop);
+        add(controls, BorderLayout.PAGE_END);
 
         setVisible(true);
     }
@@ -65,7 +65,7 @@ class UserInterface extends JFrame {
 
         DefaultTableCellRenderer renderer;
 
-        public HeaderRenderer(JTable table) {
+        HeaderRenderer(JTable table) {
             renderer = (DefaultTableCellRenderer)
                     table.getTableHeader().getDefaultRenderer();
             renderer.setHorizontalAlignment(JLabel.LEFT);
@@ -77,6 +77,17 @@ class UserInterface extends JFrame {
                 boolean hasFocus, int row, int col) {
             return renderer.getTableCellRendererComponent(
                     table, value, isSelected, hasFocus, row, col);
+        }
+    }
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getActionCommand().equalsIgnoreCase("Play")) {
+                System.out.println("Play button pressed.");
+            } else if (e.getActionCommand().equalsIgnoreCase("Stop")) {
+                System.out.println("Stop button pressed.");
+            }
         }
     }
 }
